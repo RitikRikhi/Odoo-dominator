d0bfd711f5adf0c53aaca693e1e599ce13632408
Ritik rikhi <rikhiritik8@gmail.com>
2025-08-30 14:16:23 +0530

    Delete config directory

--- config/db.js
+++
@@ -1,8 +0,0 @@
-//db setup
-
-const mongoose = require('mongoose');
-const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log('MongoDB connected');
-};
-module.exports = connectDB;
--- config/razorpay.js
+++
@@ -1,6 +0,0 @@
-//Razorpay setup
-const Razorpay = require('razorpay');
-module.exports = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY,
-  key_secret: process.env.RAZORPAY_SECRET
-});
